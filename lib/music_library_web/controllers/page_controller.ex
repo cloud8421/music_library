d547ee6f8d30269ff6c230d61552e44d39fc9618
Claudio Ortolina <cloud8421@gmail.com>
2024-09-12 14:05:08 +0100

    Generate project
    Using `mix phx.new music_library --database sqlite3 --binary-id --no-mailer`

---
+++ lib/music_library_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule MusicLibraryWeb.PageController do
+  use MusicLibraryWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
---
+++ lib/music_library_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule MusicLibraryWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use MusicLibraryWeb, :html
+
+  embed_templates "page_html/*"
+end
