e3ff0228cd26d9287bd212206995ce0f77761e33
Claudio Ortolina <cloud8421@gmail.com>
2024-09-15 16:16:01 +0100

    Load artists when showing a record

--- lib/music_library/records.ex
+++ lib/music_library/records.ex
@@ -37,7 +37,15 @@ defmodule MusicLibrary.Records do
       ** (Ecto.NoResultsError)
 
   """
-  def get_record!(id), do: Repo.get!(Record, id)
+  def get_record!(id) do
+    q =
+      from r in Record,
+        left_join: a in assoc(r, :artists),
+        preload: [artists: a],
+        where: r.id == ^id
+
+    Repo.one!(q)
+  end
 
   @doc """
   Creates a record.
