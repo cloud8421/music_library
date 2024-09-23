e666b247e1e352605e8ed6f88993bf49647238cd
Claudio Ortolina <cloud8421@gmail.com>
2024-09-23 14:09:21 +0100

    Improve display of single album page

--- lib/music_library_web/components/core_components.ex
+++ lib/music_library_web/components/core_components.ex
@@ -427,12 +427,12 @@ defmodule MusicLibraryWeb.CoreComponents do
 
   def header(assigns) do
     ~H"""
-    <header class={[@actions != [] && "flex items-center justify-between gap-6", @class]}>
+    <header class={[@actions != [] && "flex items-center justify-between gap-6 mb-2", @class]}>
       <div>
         <h1 class="text-lg font-semibold leading-8 text-zinc-800">
           <%= render_slot(@inner_block) %>
         </h1>
-        <p :if={@subtitle != []} class="mt-2 text-sm leading-6 text-zinc-600">
+        <p :if={@subtitle != []} class="mt-2 text-base leading-6 text-zinc-600">
           <%= render_slot(@subtitle) %>
         </p>
       </div>
