b0b39671bc2715851f0a5ca7f95e02a47e5bab3e
Claudio Ortolina <cloud8421@gmail.com>
2024-09-30 10:31:05 +0100

    Only use etags for caching cover art
    Preps the ground for custom uploads

--- lib/music_library_web/controllers/image_controller.ex
+++ lib/music_library_web/controllers/image_controller.ex
@@ -3,8 +3,6 @@ defmodule MusicLibraryWeb.ImageController do
 
   alias MusicLibrary.Records
 
-  @one_year 31_536_000
-
   def show(conn, %{"record_id" => record_id}) do
     case Records.get_image!(record_id) do
       nil ->
@@ -21,7 +19,7 @@ defmodule MusicLibraryWeb.ImageController do
           _ ->
             conn
             |> put_resp_content_type("image/jpeg", "utf-8")
-            |> put_resp_header("cache-control", "public, max-age=#{@one_year}")
+            |> put_resp_header("etag", etag)
             |> send_resp(200, image_data)
         end
     end
