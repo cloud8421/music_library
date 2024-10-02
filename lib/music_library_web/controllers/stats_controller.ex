a02e6a5fcf6048229c224fde437c8696318aa5de
Claudio Ortolina <cloud8421@gmail.com>
2024-10-02 21:27:38 +0100

    Rename record to latest record in stats

--- lib/music_library_web/controllers/stats_controller.ex
+++ lib/music_library_web/controllers/stats_controller.ex
@@ -6,12 +6,12 @@ defmodule MusicLibraryWeb.StatsController do
   def index(conn, _params) do
     records_count_by_format = Records.count_records_by_format()
     records_count = Enum.reduce(records_count_by_format, 0, fn {_, count}, acc -> acc + count end)
-    record = Records.get_latest_record!()
+    latest_record = Records.get_latest_record!()
 
     render(conn, :index,
       records_count_by_format: records_count_by_format,
       records_count: records_count,
-      record: record,
+      latest_record: latest_record,
       nav_section: :dashboard
     )
   end
