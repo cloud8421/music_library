8e654fb78285efc37b38c9de8e53d964c9c04b7b
Claudio Ortolina <cloud8421@gmail.com>
2024-10-22 18:14:15 +0100

    Rename counts in stats

--- lib/music_library_web/controllers/stats_controller.ex
+++ lib/music_library_web/controllers/stats_controller.ex
@@ -4,23 +4,25 @@ defmodule MusicLibraryWeb.StatsController do
   alias MusicLibrary.Records
 
   def index(conn, _params) do
-    records_count_by_format =
+    collection_count_by_format =
       Records.count_records_by_format()
       |> Enum.sort_by(fn {_format, count} -> count end, :desc)
 
-    records_count_by_type =
+    collection_count_by_type =
       Records.count_records_by_type()
       |> Enum.sort_by(fn {_type, count} -> count end, :desc)
 
-    records_count = Enum.reduce(records_count_by_format, 0, fn {_, count}, acc -> acc + count end)
+    collection_count =
+      Enum.reduce(collection_count_by_format, 0, fn {_, count}, acc -> acc + count end)
+
     latest_record = Records.get_latest_record!()
 
     conn
     |> assign(:page_title, gettext("Stats"))
     |> render(:index,
-      records_count_by_format: records_count_by_format,
-      records_count_by_type: records_count_by_type,
-      records_count: records_count,
+      collection_count_by_format: collection_count_by_format,
+      collection_count_by_type: collection_count_by_type,
+      collection_count: collection_count,
       latest_record: latest_record,
       nav_section: :stats
     )
