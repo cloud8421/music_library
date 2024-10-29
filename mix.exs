f23031821cc68a9a121a816369c9a23e0580c184
Claudio Ortolina <cloud8421@gmail.com>
2024-10-29 21:07:50 +0000

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule MusicLibrary.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:yaml_elixir, "~> 2.11"},
       {:finch, "~> 0.19.0"},
-      {:vix, "~> 0.30.0"},
+      {:vix, "~> 0.31.1"},
       {:nimble_parsec, "~> 1.4"},
       # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
       {:phoenix_live_view, "~> 1.0.0-rc.7", override: true},
