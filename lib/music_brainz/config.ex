39b925676f70725d4dd12ee248116dc7023ddc54
Claudio Ortolina <cloud8421@gmail.com>
2024-12-04 15:01:52 +0000

    Use NimbleOptions to validate MusicBrainz.Config

--- lib/music_brainz/config.ex
+++ lib/music_brainz/config.ex
@@ -7,9 +7,24 @@ defmodule MusicBrainz.Config do
   defstruct api: MusicBrainz.APIImpl,
             user_agent: "change me"
 
+  @schema NimbleOptions.new!(
+            api: [
+              type: :atom,
+              required: true
+            ],
+            user_agent: [
+              type: :string,
+              required: false,
+              default: "change me"
+            ]
+          )
+
+  @doc NimbleOptions.docs(@schema)
   @spec resolve(atom) :: t
   def resolve(otp_app) do
-    app_config = Application.get_env(otp_app, MusicBrainz)
+    app_config =
+      Application.get_env(otp_app, MusicBrainz)
+      |> NimbleOptions.validate!(@schema)
 
     struct(__MODULE__, app_config)
   end
