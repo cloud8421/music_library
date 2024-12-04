8c5f199773e0c0d612952ab3b5859256c8083f79
Claudio Ortolina <cloud8421@gmail.com>
2024-12-04 14:59:08 +0000

    Add nimble_options as a direct dependency

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule MusicLibrary.MixProject do
       {:finch, "~> 0.19.0"},
       {:vix, "~> 0.31.1"},
       {:nimble_parsec, "~> 1.4"},
+      {:nimble_options, "~> 1.1"},
       {:phoenix_live_view, "~> 1.0.0"},
       {:phoenix_html_helpers, "~> 1.0"},
       {:floki, ">= 0.30.0", only: :test},
