06fd591e5f2bc32112fc971bd1389988b27aec42
Claudio Ortolina <cloud8421@gmail.com>
2024-12-07 19:38:10 +0100

    Use unicode extension to normalize sorting accented records

--- lib/music_library/repo.ex
+++ lib/music_library/repo.ex
@@ -11,26 +11,29 @@ defmodule MusicLibrary.Repo do
   end
 
   def extension_path(name) do
-    [arch | _rest] = :erlang.system_info(:system_architecture)
+    [arch | _rest] =
+      :erlang.system_info(:system_architecture)
       |> List.to_string()
       |> String.split("-")
 
     {_, os} = :os.type()
 
-    platform = case {os, arch} do
-      {:darwin, "x86_64"} -> "darwin-amd64"
-      {:darwin, "aarch64"} -> "darwin-aarch64"
-      {:linux, "x86_64"} -> "linux-amd64"
-      _other -> raise "Unsupported OS or platform"
-    end
+    platform =
+      case {os, arch} do
+        {:darwin, "x86_64"} -> "darwin-amd64"
+        {:darwin, "aarch64"} -> "darwin-aarch64"
+        {:linux, "x86_64"} -> "linux-amd64"
+        _other -> raise "Unsupported OS or platform"
+      end
 
-    extension = case os do
-      :darwin -> "dylib"
-      :linux -> "so"
-    end
+    extension =
+      case os do
+        :darwin -> "dylib"
+        :linux -> "so"
+      end
 
     Path.join([
-     :code.priv_dir(:music_library),
+      :code.priv_dir(:music_library),
       "sqlite_extensions",
       platform,
       "#{name}.#{extension}"
