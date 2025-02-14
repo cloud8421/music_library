a8cffb08cf46f306048f0d267cfcb276b8bd89ea
Claudio Ortolina <cloud8421@gmail.com>
2025-02-14 07:40:34 +0000

    Formalize search_release_by_barcode/2 callback

--- lib/music_brainz/api_behaviour.ex
+++ lib/music_brainz/api_behaviour.ex
@@ -2,6 +2,7 @@ defmodule MusicBrainz.APIBehaviour do
   @type musicbrainz_id :: String.t()
   @type config :: MusicBrainz.Config.t()
   @type limit :: pos_integer()
+  @type barcode :: String.t()
 
   @callback get_release_group(musicbrainz_id, config) :: {:ok, map()} | {:error, String.t()}
 
@@ -11,6 +12,8 @@ defmodule MusicBrainz.APIBehaviour do
 
   @callback search_release_group(String.t(), Keyword.t(), config) ::
               {:ok, [map()]} | {:error, String.t()}
+
+  @callback search_release_by_barcode(barcode, config) :: {:ok, [map()]} | {:error, String.t()}
 
   @callback get_cover_art({:musicbrainz_id, musicbrainz_id()} | {:url, String.t()}, config) ::
               {:ok, binary()} | {:error, String.t()}
