ce5c6f1e3c5068b20dc57f9763b5f486e3a88ee2
Claudio Ortolina <cloud8421@gmail.com>
2025-02-17 21:39:49 +0000

    Simplify test_helper logic

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,13 +1,18 @@
 Mox.defmock(MusicBrainz.APIBehaviourMock, for: MusicBrainz.APIBehaviour)
-Application.put_env(:music_library, :musicbrainz, MusicBrainz.APIBehaviourMock)
-music_brainz_config = Application.get_env(:music_library, MusicBrainz)
-new_music_brainz_config = Keyword.put(music_brainz_config, :api, MusicBrainz.APIBehaviourMock)
-Application.put_env(:music_library, MusicBrainz, new_music_brainz_config)
+
+music_brainz_config =
+  Application.get_env(:music_library, MusicBrainz)
+  |> Keyword.put(:api, MusicBrainz.APIBehaviourMock)
+
+Application.put_env(:music_library, MusicBrainz, music_brainz_config)
 
 Mox.defmock(LastFm.APIBehaviourMock, for: LastFm.APIBehaviour)
-last_fm_config = Application.get_env(:music_library, LastFm)
-new_last_fm_config = Keyword.put(last_fm_config, :api, LastFm.APIBehaviourMock)
-Application.put_env(:music_library, LastFm, new_last_fm_config)
+
+last_fm_config =
+  Application.get_env(:music_library, LastFm)
+  |> Keyword.put(:api, LastFm.APIBehaviourMock)
+
+Application.put_env(:music_library, LastFm, last_fm_config)
 
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(MusicLibrary.Repo, :manual)
