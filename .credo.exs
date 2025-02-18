631552b48777628bceb135f80856f771a593d72a
Claudio Ortolina <cloud8421@gmail.com>
2025-02-18 12:56:29 +0000

    Turn off unneeded Credo checks

---
+++ .credo.exs
@@ -0,0 +1,16 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      checks: %{
+        disabled: [
+          {Credo.Check.Refactor.CyclomaticComplexity, []},
+          {Credo.Check.Refactor.Nesting, []},
+          {Credo.Check.Readability.ModuleDoc, []},
+          {Credo.Check.Design.TagTODO, []}
+        ]
+      }
+      # files etc.
+    }
+  ]
+}
