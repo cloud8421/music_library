4eac9f715a2e58bdabd7d054a9720e16f9e0e1b2
Claudio Ortolina <cloud8421@gmail.com>
2025-04-16 10:42:37 +0100

    Mount Fluxon UI inside css and js entrypoints

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,10 +21,11 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
+import { Hooks as FluxonHooks, DOM as FluxonDOM } from "fluxon";
 import BarcodeScannerHook from "./barcode-scanner";
 import confetti from "canvas-confetti";
 
-let Hooks = {};
+let Hooks = FluxonHooks;
 Hooks.BarcodeScanner = BarcodeScannerHook;
 
 let csrfToken = document
@@ -34,6 +35,11 @@ let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
   hooks: Hooks,
+  dom: {
+    onBeforeElUpdated(from, to) {
+      FluxonDOM.onBeforeElUpdated(from, to);
+    },
+  },
 });
 
 // Show progress bar on live navigation and form submits
