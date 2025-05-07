9aecd4cc944b2cc96e1c78c7e7c7a48cc01c702f
Claudio Ortolina <cloud8421@gmail.com>
2025-05-07 09:03:47 +0100

    Fix last_fm_session_key secret name

--- lib/music_library_web/controllers/last_fm_controller.ex
+++ lib/music_library_web/controllers/last_fm_controller.ex
@@ -5,7 +5,7 @@ defmodule MusicLibraryWeb.LastFmController do
 
   def callback(conn, %{"token" => token}) do
     with {:ok, session} <- LastFm.get_session(token),
-         {:ok, _secret} <- Secrets.store("last_fm_key", session.key) do
+         {:ok, _secret} <- Secrets.store("last_fm_session_key", session.key) do
       conn
       |> put_flash(:info, gettext("Successfully connected your Last.fm account"))
       |> redirect(to: ~p"/")
