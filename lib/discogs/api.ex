83fb9a01b359fabef8b5ed22d29a7addb15cc106
Claudio Ortolina <cloud8421@gmail.com>
2025-05-23 15:08:12 +0100

    Log Discogs API errors

--- lib/discogs/api.ex
+++ lib/discogs/api.ex
@@ -32,6 +32,7 @@ defmodule Discogs.API do
     )
     |> Req.Request.merge_options(config.req_options)
     |> Req.Request.append_request_steps(log_attempt: &log_attempt/1)
+    |> Req.Request.append_response_steps(log_error: &log_error/1)
   end
 
   defp get_request(request) do
