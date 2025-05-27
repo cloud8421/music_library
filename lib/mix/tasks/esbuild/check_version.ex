c0ef2038e7ac6466c5e7974f03b72ad35fcf3562
Claudio Ortolina <cloud8421@gmail.com>
2025-05-27 14:49:24 +0100

    Add tasks to update esbuild version

---
+++ lib/mix/tasks/esbuild/check_version.ex
@@ -0,0 +1,28 @@
+defmodule Mix.Tasks.Esbuild.CheckVersion do
+  use Mix.Task
+
+  @shortdoc "Checks the the current Esbuild version is the latest"
+  @moduledoc """
+  Checks the the current esbuild version is the latest.
+
+  Exits with 0 if versions match, 1 if the esbuild needs to be updated.
+  """
+
+  alias Mix.Tasks.Esbuild.Release
+
+  @impl Mix.Task
+  def run(_args) do
+    current_version = Release.fetch_current_version()
+    latest_version = Release.fetch_latest_version!()
+
+    if current_version !== latest_version do
+      Mix.Shell.IO.info(
+        "A new esbuild version is available: #{current_version} ==> #{latest_version}"
+      )
+
+      System.halt(1)
+    else
+      Mix.Shell.IO.info("The esbuild version is up to date (#{current_version})")
+    end
+  end
+end
---
+++ lib/mix/tasks/esbuild/update_version.ex
@@ -0,0 +1,29 @@
+defmodule Mix.Tasks.Esbuild.UpdateVersion do
+  use Mix.Task
+
+  @shortdoc "Update the configured esbuild versino to latest."
+  @moduledoc """
+  Update the configured esbuild versino to latest.
+  """
+
+  alias Mix.Tasks.Esbuild.Release
+
+  @impl Mix.Task
+  def run(_args) do
+    current_version = Release.fetch_current_version()
+    latest_version = Release.fetch_latest_version!()
+
+    if current_version !== latest_version do
+      Mix.Shell.IO.info(
+        "Updating esbuild configuration from #{current_version} to #{latest_version}"
+      )
+
+      config_file =
+        Path.expand("config/config.exs", File.cwd!())
+
+      Release.update_config_file!(config_file, current_version, latest_version)
+    else
+      Mix.Shell.IO.info("esbuild configuration is up to date (#{current_version})")
+    end
+  end
+end
