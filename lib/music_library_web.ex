f95843dae15c21b9be90bd4ab78dde8e33c84a53
Claudio Ortolina <cloud8421@gmail.com>
2025-06-11 09:19:02 +0300

    Serve apple-touch-icons

--- lib/music_library_web.ex
+++ lib/music_library_web.ex
@@ -17,7 +17,9 @@ defmodule MusicLibraryWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths,
+    do:
+      ~w(assets fonts images favicon.ico robots.txt apple-touch-icon-precomposed.png apple-touch-icon.png)
 
   def router do
     quote do
