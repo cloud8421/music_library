c591d39f52550c19770e68ad4080e5614caf3782
Claudio Ortolina <cloud8421@gmail.com>
2025-06-25 20:25:25 +0300

    Update dependencies
    live_debugger 0.2.0 => 0.3.0

--- mix.exs
+++ mix.exs
@@ -74,7 +74,7 @@ defmodule MusicLibrary.MixProject do
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:quokka, "~> 2.6", only: [:dev, :test], runtime: false},
-      {:live_debugger, "~> 0.2.0", only: :dev},
+      {:live_debugger, "~> 0.3.0", only: :dev},
       {:tidewave, "~> 0.1", only: :dev},
 
       # HTTP Clients
