a989418125d9d0aa0155beb1672e70bb88b21cc6
Claudio Ortolina <cloud8421@gmail.com>
2025-09-05 10:30:26 +0300

    Update tailwind from 4.1.12 to 4.1.13

--- config/config.exs
+++ config/config.exs
@@ -57,7 +57,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.1.12",
+  version: "4.1.13",
   music_library: [
     args: ~w(
       --input=assets/css/app.css
