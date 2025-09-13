4314ed6cfc6bdab3dba99e3bc97f7aa577c39866
Claudio Ortolina <cloud8421@gmail.com>
2025-09-13 12:19:57 +0300

    Add ability to fetch sqlean release URLs

--- lib/mix/tasks/sqlean/release.ex
+++ lib/mix/tasks/sqlean/release.ex
@@ -1,13 +1,10 @@
 defmodule Mix.Tasks.Sqlean.Release do
-  @latest_version_url "https://raw.githubusercontent.com/nalgeon/sqlean/refs/heads/main/sqlpkg.json"
+  @latest_version_info_url "https://raw.githubusercontent.com/nalgeon/sqlean/refs/heads/main/sqlpkg.json"
 
   def fetch_latest_version! do
     Application.ensure_all_started(:req)
 
-    @latest_version_url
-    |> Req.get!()
-    |> Map.get(:body)
-    |> JSON.decode!()
+    fetch_latest_version_info!()
     |> Map.get("version")
   end
 
@@ -15,6 +12,32 @@ defmodule Mix.Tasks.Sqlean.Release do
     version_file()
     |> File.read!()
     |> String.trim()
+  end
+
+  # https://github.com/nalgeon/sqlean/releases/download/0.28.0/sqlean-linux-arm64.zip
+  def fetch_release_urls! do
+    Application.ensure_all_started(:req)
+
+    assets =
+      fetch_latest_version_info!()
+      |> get_in(["assets", "files"])
+
+    version = fetch_latest_version!()
+
+    Enum.map(assets, fn {arch, file} ->
+      {arch, full_url(version, file)}
+    end)
+  end
+
+  defp fetch_latest_version_info! do
+    @latest_version_info_url
+    |> Req.get!()
+    |> Map.get(:body)
+    |> JSON.decode!()
+  end
+
+  defp full_url(version, file) do
+    "https://github.com/nalgeon/sqlean/releases/download/" <> version <> "/" <> file
   end
 
   def version_file do
