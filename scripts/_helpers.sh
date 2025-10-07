c513c43a5241310591b7363abf923475ca7959f4
Claudio Ortolina <cloud8421@gmail.com>
2025-10-07 10:30:38 +0300

    Ensure working directory in scripts
    So that they can be run standalone and not just through Mise

---
+++ scripts/_helpers.sh
@@ -0,0 +1,8 @@
+ensure_working_directory!() {
+  root_dir="$(git rev-parse --show-toplevel)"
+
+  if [[ "$PWD" != "$root_dir" ]]; then
+    echo "Please run the script from the root of the repo: $root_dir"
+    exit
+  fi
+}
