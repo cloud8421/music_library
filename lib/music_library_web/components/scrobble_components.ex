31e86e1b41b5f02621146ba998274102a9c9975d
Claudio Ortolina <cloud8421@gmail.com>
2025-10-21 15:11:39 +0100

    Fix offset animation on Last.fm refresh icon

--- lib/music_library_web/components/scrobble_components.ex
+++ lib/music_library_web/components/scrobble_components.ex
@@ -9,11 +9,16 @@ defmodule MusicLibraryWeb.ScrobbleComponents do
     ~H"""
     <button
       type="button"
-      class="phx-click-loading:animate-spin text-zinc-500 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-300"
+      class="text-zinc-500 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-300"
       phx-click={JS.push("refresh_lastfm_feed")}
     >
       <span class="sr-only">{gettext("Refresh LastFm Feed")}</span>
-      <.icon name="hero-arrow-path" class="h-5 w-5" aria-hidden="true" data-slot="icon" />
+      <.icon
+        name="hero-arrow-path"
+        class="phx-click-loading:animate-spin h-5 w-5"
+        aria-hidden="true"
+        data-slot="icon"
+      />
     </button>
     """
   end
