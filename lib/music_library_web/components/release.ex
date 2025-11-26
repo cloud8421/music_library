e17c5d05adf9421939e44b22b8e66072761506cc
Claudio Ortolina <cloud8421@gmail.com>
2025-11-26 13:22:04 +0000

    Show medium format near medium name

--- lib/music_library_web/components/release.ex
+++ lib/music_library_web/components/release.ex
@@ -146,6 +146,9 @@ defmodule MusicLibraryWeb.Components.Release do
           class="w-4 h-4 mr-2 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
         />
         {medium_title(@medium)}
+        <.badge variant="soft" class="text-xs">
+          {@medium.format}
+        </.badge>
       </h4>
       <.button
         :if={@can_scrobble?}
