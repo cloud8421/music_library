f6cd2d70761d8a4815a4aff0b801c73abd50eaf7
Claudio Ortolina <cloud8421@gmail.com>
2025-12-17 12:05:41 +0300

    Update esbuild from 0.27.1 to 0.27.2

--- config/config.exs
+++ config/config.exs
@@ -49,7 +49,7 @@ config :music_library, Discogs, personal_access_token: "change me", user_agent: 
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.27.1",
+  version: "0.27.2",
   music_library: [
     args:
       ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --alias:@=.),
