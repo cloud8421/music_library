39fb59835eaaec69e53ade0430dd264bb5cdf901
Claudio Ortolina <cloud8421@gmail.com>
2026-01-23 14:45:00 +0000

    Update dependencies
    vix 0.36.0 => 0.37.0 (minor)

--- mix.exs
+++ mix.exs
@@ -92,7 +92,7 @@ defmodule MusicLibrary.MixProject do
       {:nimble_parsec, "~> 1.4"},
 
       # Image manipulation
-      {:vix, "~> 0.36.0"},
+      {:vix, "~> 0.37.0"},
 
       # Notes
       {:earmark, "~> 1.4"},
