7dd35c2745cb09ab94da2efd1302046994dd6820
Claudio Ortolina <cloud8421@gmail.com>
2026-01-27 21:20:30 +0000

    Remove unused component

--- lib/music_library_web/components/add_record.ex
+++ lib/music_library_web/components/add_record.ex
@@ -117,16 +117,6 @@ defmodule MusicLibraryWeb.Components.AddRecord do
     """
   end
 
-  attr :type, :string, required: true
-
-  defp type_badge(assigns) do
-    ~H"""
-    <span class="inline-flex items-center rounded-md bg-zinc-50 px-2 py-1 text-xs font-medium text-zinc-600 ring-1 ring-inset ring-zinc-500/10">
-      {@type}
-    </span>
-    """
-  end
-
   @impl true
   def mount(socket) do
     {:ok,
