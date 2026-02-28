2f57ce2e673ff061c36a9af3fdb4aa3a5dcfd091
Claudio Ortolina <cloud8421@gmail.com>
2026-02-28 10:30:44 +0000

    Display static changed banner instead of toast

--- lib/music_library_web/hooks/static_assets.ex
+++ lib/music_library_web/hooks/static_assets.ex
@@ -2,13 +2,6 @@ defmodule MusicLibraryWeb.Hooks.StaticAssets do
   use MusicLibraryWeb, :live_component
 
   def on_mount(:default, _params, _session, socket) do
-    socket =
-      if static_changed?(socket) do
-        put_toast(socket, :warning, gettext("The application has been updated, please reload."))
-      else
-        socket
-      end
-
-    {:cont, socket}
+    {:cont, assign(socket, :static_changed, static_changed?(socket))}
   end
 end
