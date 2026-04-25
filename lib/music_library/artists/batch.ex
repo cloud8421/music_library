4fb1bf507916087308c91b3b3b10e5647039b82f
Claudio Ortolina <cloud8421@gmail.com>
2026-04-25 21:34:11 +0100

    Remove duplication in implementation

--- lib/music_library/artists/batch.ex
+++ lib/music_library/artists/batch.ex
@@ -11,29 +11,25 @@ defmodule MusicLibrary.Artists.Batch do
 
   @spec refresh_musicbrainz_data() :: {:ok, [String.t()]}
   def refresh_musicbrainz_data do
-    Batch.run_on_all(from(r in ArtistInfo), "artist_info", fn artist_info ->
-      Artists.refresh_musicbrainz_data_async(artist_info)
-    end)
+    batch(&Artists.refresh_musicbrainz_data_async/1)
   end
 
   @spec refresh_discogs_data() :: {:ok, [String.t()]}
   def refresh_discogs_data do
-    Batch.run_on_all(from(r in ArtistInfo), "artist_info", fn artist_info ->
-      Artists.refresh_discogs_data_async(artist_info)
-    end)
+    batch(&Artists.refresh_discogs_data_async/1)
   end
 
   @spec refresh_wikipedia_data() :: {:ok, [String.t()]}
   def refresh_wikipedia_data do
-    Batch.run_on_all(from(r in ArtistInfo), "artist_info", fn artist_info ->
-      Artists.refresh_wikipedia_data_async(artist_info)
-    end)
+    batch(&Artists.refresh_wikipedia_data_async/1)
   end
 
   @spec refresh_lastfm_data() :: {:ok, [String.t()]}
   def refresh_lastfm_data do
-    Batch.run_on_all(from(r in ArtistInfo), "artist_info", fn artist_info ->
-      Artists.refresh_lastfm_data_async(artist_info)
-    end)
+    batch(&Artists.refresh_lastfm_data_async/1)
+  end
+
+  defp batch(callback) do
+    Batch.run_on_all(from(r in ArtistInfo), "artist_info", callback)
   end
 end
