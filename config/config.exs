512a34b8dc1cb9e7aa14bafee9a700e8b39396a3
Claudio Ortolina <cloud8421@gmail.com>
2026-06-12 21:47:43 +0300

    Update dependencies
      tailwind 4.3.0 => 4.3.1
    
    mix:
      lumis 0.5.0 => 0.6.0

--- config/config.exs
+++ config/config.exs
@@ -90,7 +90,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.3.0",
+  version: "4.3.1",
   music_library: [
     args: ~w(
       --input=assets/css/app.css
